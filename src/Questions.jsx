e89401e4514cab7f6c9efc61cfddb1f4fd33f3e2
ddaarinne <koliada2209@gmail.com>
2026-03-04 11:08:11 +0200

    alternativw

--- src/Questions.jsx
+++ src/Questions.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import SingleQuestion from "./SingleQuestion";
+import SingleQuestion from "./SingleQuestion";
 
 const Questions = ({ questions }) => {
   return (
     <section className="container">
       <h1>Questions</h1>
       {questions.map((question) => {
-        return <SingleQuestion key={question.id} {...question} />;
+        return (
+          <SingleQuestion
+            key={question.id}
+            {...question}
+            activeId={activeId}
+            toggleQuestion={toggleQuestion}
+          ></SingleQuestion>
+        );
       })}
     </section>
   );
